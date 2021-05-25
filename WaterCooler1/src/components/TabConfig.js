7590fa3e314cc135fd197e9e40b7822637dbafe5
Alejandro Brito <69440796+alejandro-brito@users.noreply.github.com>
2021-05-24 22:36:46 -0400

    Update TabConfig.js

--- WaterCooler1/src/components/TabConfig.js
+++ WaterCooler1/src/components/TabConfig.js
@@ -6,9 +6,6 @@ import logo from "../icon.png"
 import React from 'react';
 import '../App.css';
 import * as microsoftTeams from "@microsoft/teams-js";
-import CreateDepartment from '../Pages/CreateDepartment'
-import { Link } from 'react-router-dom'
-
 
 /**
  * The 'Config' component is used to display your group tabs
@@ -50,9 +47,7 @@ class TabConfig extends React.Component {
           <img src={logo} alt="Water Cooler Logo"/>
           <div className="container">
             <p>Water Cooler's goal is to make geographically distributed teams feel more connected. At a quick glimpse, members of an organization can look at the Water Cooler app and see everyone in their organization, the departments/groups they belong to, and a customizable description of someone. It allows members to connect a name to a person very quickly and learn about them, all in a single app!</p>           
-            <p>Start by clicking the "Create Department" button below and creating your first group/department! This app automatically creates a list using this current team's roster. When you're finished, click the "Homepage" button. If you ever need to add/delete/update your departments, you can do so by logging into the admin page. <br /> (first time admin login credentials [username:root pass:Hack.Diversity])</p>
-            <CreateDepartment />
-            <Link to="/" className="home-button">Homepage</Link>
+            <p>After closing this tab, go to the login page by clicking the "Admin" button. After logging in, click the "Create Department" button in the lower right corner. This app automatically creates a list using this current team's roster. When you're finished creating departments, click the "Homepage" button. If you ever need to add/delete/update your departments, just head to the admin page. <br /> (first time admin login credentials [username:root pass:Hack.Diversity])</p>
           </div>
         </div>
       );
